7ebdd94fa21b52406e01728db6f35721b993d7c6
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2023-11-21 10:07:52 -0300

    RF-9: user-info CRUD

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import exerciseRouter from './exercise';
+import userInfoRouter from './userInfo';
 
 const router = express.Router();
 
 router.use('/exercise', exerciseRouter);
+router.use('/user-info', userInfoRouter);
 
 export default router;
