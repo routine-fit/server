bab4eb7ca1c818aed4ebc825ff22dc770f6e5ac1
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2023-11-21 11:29:09 -0300

    RF-10: training preferences CRUD

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import exerciseRouter from './exercise';
+import trainingPreferenceRouter from './training-preferences';
 
 const router = express.Router();
 
 router.use('/exercise', exerciseRouter);
+router.use('/training-preference', trainingPreferenceRouter);
 
 export default router;
